0b9164a0463a3b05257d9994147b665232efd091
Seungwoo Hong <70307275+seungwoo-h@users.noreply.github.com>
2021-05-26 23:24:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+setup( 
+    name ='econometricsstats', 
+    version = '1.0.0', 
+    description = 'stats wrapper for econometrics', 
+    author = 'sw', 
+    author_email = None,
+    install_requires=['linearmodels==4.24'],
+    url = None, 
+    py_modules = ['econometrics_stats']
+ )
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="econometrics_calculator",
-    version="1.0",
-    author="swxxxxxx",
-    author_email="anonymous@gmail.com",
-    description="Statsmodel wrapper",
-    long_description=long_description,
-    url="https://github.com/seungwoo-h/econometrics_calculator",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=requirements,
